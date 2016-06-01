b18cd9ca67d736b3319eb6a55e5803b6d8d88b49
mariocova3 <cm10079253@011-a218-37582.lisd.local>
2016-06-01 14:58:44 -0500

    End of Class 6/1.

---
+++ Assets/Scripts/MobileInputScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileInputScript : MonoBehaviour 
+{
+
+	public float speed = 15;
+	public float jump = 3;
+
+	CharacterController control;
+
+	// Use this for initialization
+	void Start () 
+	{
+		control = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float x = Input.acceleration.x * Time.deltaTime * speed;
+		float z = -Input.acceleration.z * Time.deltaTime * speed;
+
+		transform.Translate (x, 0f, z);
+
+		foreach (Touch touch in Input.touches) 
+		{
+
+			//turning
+			if (touch.position.x > Screen.width / 2) 
+			{
+				transform.Rotate (0f, touch.deltaPosition.x, 0f);
+			}
+		}
+	}
+}
